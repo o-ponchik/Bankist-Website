0550f93a0bbd94cc442ce5abf1d28341ca18cb92
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-11-28 16:28:45 +0200

    added_page_navigation_to_navbar

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 "use strict";
 
-///////////////////////////////////////
-// Modal window - open and close function ///////////////////////////////////////////////////////////
-
+//------------------Selections----------------------------
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const allSections = document.querySelectorAll(".section");
+const header = document.querySelector(".header");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
+///////////////////////-----Scrolling to first section-----////////////////////////////////////
+// Smooth scrolling to the section
+
+btnScrollTo.addEventListener("click", function (e) {
+  //Scrolling
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+/////////////////////////-----Cookies message-------/////////////////////////
+// Creating and inserting cookies message
+const message = document.createElement("div");
+
+message.classList.add("cookie-message");
+message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+header.after(message);
+
+// Delete cookies message
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+// Style cookies message
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+////////////////////----------Modal window--------/////////////////////////
+// Modal window - open and close function //////////////////////////////////////////////////////////
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
@@ -31,35 +65,18 @@ document.addEventListener("keydown", function (e) {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
-const allSections = document.querySelectorAll(".section");
-const header = document.querySelector(".header");
 
-// Creating and inserting cookies message
-const message = document.createElement("div");
+///////////////////-----Page navigation------/////////////////////////
+//Event delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
 
-message.classList.add("cookie-message");
-message.innerHTML = `We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
-header.after(message);
-
-// Delete cookies message
-document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
-    message.remove();
-  });
-
-// Style cookies message
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
-
-// Smooth scrolling to the section
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
-btnScrollTo.addEventListener("click", function (e) {
-  //Scrolling
-  section1.scrollIntoView({ behavior: "smooth" });
+  //Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
 });
