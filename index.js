1636dfa03cb427be6c2f62738195851e74fb02fb
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-11-29 00:55:15 +0200

    implemented_lazy_loading_images

--- index.js
+++ index.js
@@ -143,7 +143,6 @@ headerObserver.observe(header);
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -160,3 +159,30 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+////////////////////////----Lazy loading images---//////////////////////
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  // remuve blure-effect after loading img
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "100px",
+});
+imgTargets.forEach((img) => imgObserver.observe(img));
